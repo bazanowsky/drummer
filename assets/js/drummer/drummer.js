81725da53a673388a5ab50ccbba57ae1464156a6
Bazan <marcin@bazanowski.pl>
2015-09-09 19:06:26 +0200

    Init

---
+++ assets/js/drummer/drummer.js
@@ -0,0 +1,10 @@
+
+var Drummer  = function(canvas){
+	this.canvas = canvas;
+	this.init();
+}
+
+Drummer.prototype.init = function() {
+	console.log('test');
+}
+
---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+
+var canvas = document.getElementById('playground');
+
+var Drummer = new Drummer(canvas);
